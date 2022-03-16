2f5161740bc4cd1b327c217e40af404188ae01a0
iannavpc <97460189+iannavpc@users.noreply.github.com>
2022-03-15 21:34:34 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,44 @@
+<body>
+
+
+
+<script>
+<input type="text" id="txt"></input>
+        
+        <script>
+        let x = document.getElementById("txt");
+        setTimeout(function(){ x.value="1 seconds" }, 1000);
+        setTimeout(function(){ x.value="2 seconds" }, 2000);
+        setTimeout(function(){ x.value="3 seconds" }, 3000);
+        setTimeout(function(){ x.value="4 seconds" }, 4000);
+        setTimeout(function(){ x.value="5 seconds" }, 5000);
+        setTimeout(function(){ x.value="6 seconds" }, 6000);
+        setTimeout(function(){ x.value="7 seconds" }, 7000);
+        setTimeout(function(){ x.value="8 seconds" }, 8000);
+        setTimeout(function(){ x.value="9 seconds" }, 9000);
+        setTimeout(function(){ x.value="10 seconds" }, 10000);
+
+        </script>
+        
+        <script type="text/javascript">
+  var myTimer = window.setTimeout(timer, 10000);
+
+  function timer() {
+    window.location.href= "level7wrong.html"
+  }
+
+  </script>
+  </script>
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
